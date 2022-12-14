535bcd08d33030477056d4f2910e714291bec6a8
JuwonChoi902 <qfnl90@naver.com>
2022-12-14 13:36:38 +0900

    메인창 UI완성

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import Login from '../Login/Login';
+import BillBoard from './components/BillBoard';
+import Popular from './components/Popular';
+import NeedHelp from './components/NeedHelp';
+import WannaRich from './components/WannaRich';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <OuterBox>
+      <Login />
+      <BillBoard />
+      <Popular />
+      <NeedHelp />
+      <WannaRich />
+    </OuterBox>
+  );
 }
+
+const OuterBox = styled.div``;
